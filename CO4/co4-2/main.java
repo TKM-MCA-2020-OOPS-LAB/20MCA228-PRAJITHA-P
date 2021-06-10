8d7e8318164a63b734ae43a785a6f010092414c4
PRAJITHA P <74781318+PRAJITHA-P@users.noreply.github.com>
2021-06-10 08:05:48 +0530

    course outcome 4

---
+++ CO4/co4-2/main.java
@@ -0,0 +1,19 @@
+import Arithematic.Math;
+public class Main
+{
+
+	public static void main(String[] args) 
+	{
+
+		Math obj = new Math();
+		
+		System.out.println("   ARITHMETIC OPERATIONS   ");
+    	obj.addition();
+    	obj.subtraction();
+    	obj.multiplication();
+    	obj.division();
+	}
+     
+       
+      
+}
---
+++ CO4/co4-2/math.java
@@ -0,0 +1,53 @@
+package Arithemetic;
+
+import java.util.Scanner;
+
+interface operation {
+	void addition();
+	void subtraction();
+	void multiplication();
+	void division();
+	}
+
+public class Math implements operation  {
+	public void addition() {
+		Scanner sc = new Scanner(System.in);
+    System.out.println("\nEnter the first number:");
+		int a = sc.nextInt();
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Enter the second number:");
+		int b = sc1.nextInt();
+		System.out.println("Sum is :"+(a+b));
+
+}
+public void subtraction() {
+	Scanner sc2 = new Scanner(System.in);
+		System.out.println("\nEnter the first number:");
+		int a = sc2.nextInt();
+		Scanner sc3 = new Scanner(System.in);
+		System.out.println("Enter the second number:");
+		int b = sc3.nextInt();
+		
+		System.out.println("Difference is :"+(a-b));
+		
+	}
+public void multiplication() {
+	Scanner sc4 = new Scanner(System.in);
+	System.out.println("\nEnter the first number:");
+	int a = sc4.nextInt();
+	Scanner sc5 = new Scanner(System.in);
+	System.out.println("Enter the second number:");
+	int b = sc5.nextInt();
+	System.out.println(" Product is :"+(a*b));
+}
+public void division() {
+	Scanner sc6 = new Scanner(System.in);
+	System.out.println("\nEnter the first number:");
+	int a = sc6.nextInt();
+	Scanner sc7 = new Scanner(System.in);
+	System.out.println("Enter the second number:");
+	int b = sc7.nextInt();
+	System.out.println("Quotient  is :"+(a/b));
+	
+}
+}
