727bbfdbebfaaba40871a0d4ada5acde71ec2899
PRAJITHA P <74781318+PRAJITHA-P@users.noreply.github.com>
2021-06-10 07:59:17 +0530

    course outcome1

---
+++ co4-1/graphics/Result.java
@@ -0,0 +1,14 @@
+package graphics;
+import graphics.Shape;
+import graphics.Shape.Area;
+public class Result {
+	public static void main(String[] args)
+	{
+		Area a=new Area();
+		a.circle();
+		a.triangle();
+		a.rectangle();
+		a.square();
+	}
+
+}
