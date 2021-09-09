1b77681c60bedad673c8dee9802b74c32ba6dd46
PRAJITHA P <74781318+PRAJITHA-P@users.noreply.github.com>
2021-09-09 12:57:17 +0530

    Key Events

---
+++ CO5/co5-1/co5-8/keyEvent.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.awt.*;  
+import java.awt.event.*;  
+public class Events extends Frame implements KeyListener{  
+    Label l;  
+    TextArea area;  
+    Events(){  
+         l=new Label();  
+        l.setBounds(20,50,100,20);  
+        area=new TextArea();  
+        area.setBounds(20,80,300, 300);  
+        area.addKeyListener(this);  
+          
+        add(l);add(area);  
+        setSize(400,400);  
+        setLayout(null);  
+        setVisible(true);    
+       
+    }  
+    public void keyPressed(KeyEvent e) {  
+        l.setText("Key Pressed");  
+    }  
+    public void keyReleased(KeyEvent e) {  
+        l.setText("Key Released");  
+    }  
+    public void keyTyped(KeyEvent e) {  
+        l.setText("Key Typed");  
+    }  
+  
+    public static void main(String[] args) {  
+        new Events();  
+    }  
+}  
+
+/*<applet code="KeyEvents" width=300 height=300></applet>*/
